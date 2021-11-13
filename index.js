ee4c636d7334b836b1a2aeb0be57acef820fd796
annuv231 <annuv231@gmail.com>
2021-11-13 23:01:13 +0530

    commented out two console.log() codes

--- index.js
+++ index.js
@@ -1,74 +1,82 @@
-var x,hour,min,sec
+var x, hour, min, sec;
 setInterval(() => {
-     x= new Date();
-hour=x.getHours();
- min=x.getMinutes();
-sec=x.getSeconds();
-var toggle=0;
-if(hour>12){
-  hour=hour-12;
-  toggle=1;
-}
-if(sec<10){
-    sec="0"+sec;
-}
-console.log(typeof hour);
-if(hour<10){
-    hour="0"+hour;
-}
-if(min<10){
-    min="0"+min;
-}
- if(toggle){
-document.getElementById("gettime").innerHTML=hour+":"+min+":"+sec+" PM";}
-     else {
-          document.getElementById("gettime").innerHTML=hour+":"+min+":"+sec+" AM";
-     }
+  x = new Date();
+  hour = x.getHours();
+  min = x.getMinutes();
+  sec = x.getSeconds();
+  var toggle = 0;
+  if (hour > 12) {
+    hour = hour - 12;
+    toggle = 1;
+  }
+  if (sec < 10) {
+    sec = "0" + sec;
+  }
+  // console.log(typeof hour);
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (min < 10) {
+    min = "0" + min;
+  }
+  if (toggle) {
+    document.getElementById("gettime").innerHTML =
+      hour + ":" + min + ":" + sec + " PM";
+  } else {
+    document.getElementById("gettime").innerHTML =
+      hour + ":" + min + ":" + sec + " AM";
+  }
 }, 1000);
 function toggleFullScreen() {
-    if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
-     (!document.mozFullScreen && !document.webkitIsFullScreen)) {
-      if (document.documentElement.requestFullScreen) {  
-        document.documentElement.requestFullScreen();  
-      } else if (document.documentElement.mozRequestFullScreen) {  
-        document.documentElement.mozRequestFullScreen();  
-      } else if (document.documentElement.webkitRequestFullScreen) {  
-        document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
-      }  
-    } else {  
-      if (document.cancelFullScreen) {  
-        document.cancelFullScreen();  
-      } else if (document.mozCancelFullScreen) {  
-        document.mozCancelFullScreen();  
-      } else if (document.webkitCancelFullScreen) {  
-        document.webkitCancelFullScreen();  
-      }  
-    }  
+  if (
+    (document.fullScreenElement && document.fullScreenElement !== null) ||
+    (!document.mozFullScreen && !document.webkitIsFullScreen)
+  ) {
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(
+        Element.ALLOW_KEYBOARD_INPUT
+      );
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
   }
-  var x,hour,min,sec
-  setInterval(() => {
-       x= new Date();
-  hour=x.getHours();
-   min=x.getMinutes();
-  sec=x.getSeconds();
-  var toggle=0;
-  if(hour>12){
-    hour=hour-12;
-    toggle=1;
+}
+var x, hour, min, sec;
+setInterval(() => {
+  x = new Date();
+  hour = x.getHours();
+  min = x.getMinutes();
+  sec = x.getSeconds();
+  var toggle = 0;
+  if (hour > 12) {
+    hour = hour - 12;
+    toggle = 1;
   }
-  if(sec<10){
-      sec="0"+sec;
+  if (sec < 10) {
+    sec = "0" + sec;
   }
-  console.log(typeof hour);
-  if(hour<10){
-      hour="0"+hour;
+  // console.log(typeof hour);
+  if (hour < 10) {
+    hour = "0" + hour;
   }
-  if(min<10){
-      min="0"+min;
+  if (min < 10) {
+    min = "0" + min;
   }
-   if(toggle){
-  document.getElementById("title").innerHTML=hour+":"+min+":"+sec+" PM";}
-       else {
-            document.getElementById("title").innerHTML=hour+":"+min+":"+sec+" AM";
-       }
-  }, 1000);
+  if (toggle) {
+    document.getElementById("title").innerHTML =
+      hour + ":" + min + ":" + sec + " PM";
+  } else {
+    document.getElementById("title").innerHTML =
+      hour + ":" + min + ":" + sec + " AM";
+  }
+}, 1000);
