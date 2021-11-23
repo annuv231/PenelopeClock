0b962222e8737e5461a506b7e36c5390b493931d
annuv231 <annuv231@gmail.com>
2021-11-23 16:46:30 +0530

    changes made in all files

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 'use strict';
-let x, hour, min, sec;
+let x, hour, min, sec, date, month, weekdays;
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
 setInterval(() => {
   x = new Date();
   hour = x.getHours();
   min = x.getMinutes();
   sec = x.getSeconds();
+  date = x.getDate();
+  month = x.getMonth();
+  weekdays = x.getDay();
+  weekdays = days[weekdays];
+  month = monthNames[month];
+  date = date + ' ' + month + ',' + weekdays;
   let toggle = false;
   if (hour > 12) {
     hour = hour - 12;
@@ -30,4 +59,5 @@ setInterval(() => {
     // document.getElementById("gettime").innerHTML = am;
     document.querySelector('.title').textContent = am;
   }
+  document.querySelector('.date').textContent = date;
 }, 1000);
