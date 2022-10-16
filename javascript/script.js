f1df2a719b69f3fa2e0dfcd726b3168228646303
Anu <annuv231@gmail.com>
2022-10-16 18:08:33 +0530

    added some fetaures

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,11 @@
 'use strict';
-let x, hour, min, sec, date, month, weekdays;
+const hour = document.querySelector('.hours');
+const minute = document.querySelector('.minutes');
+const second = document.querySelector('.seconds');
+const ampm = document.querySelector('.ampm');
+const date = document.querySelector('.dates');
+const weekday = document.querySelector('.weekdays');
+
 const monthNames = [
   'Jan',
   'Feb',
@@ -23,56 +29,43 @@ const days = [
   'Friday',
   'Saturday',
 ];
-setInterval(() => {
-  x = new Date();
-  hour = x.getHours();
-  min = x.getMinutes();
-  sec = x.getSeconds();
-  date = x.getDate();
-  month = x.getMonth();
-  weekdays = x.getDay();
-  weekdays = days[weekdays];
-  month = monthNames[month];
-  date = date + ' ' + month + ',' + weekdays;
-  let toggle = false;
-  if (hour > 12) {
-    hour = hour - 12;
-    toggle = 1;
+function updateDate() {
+  let hr = new Date().getHours();
+  let min = new Date().getMinutes();
+  let sec = new Date().getSeconds();
+  let dt = new Date().getDate();
+  let mn = new Date().getMonth();
+  let day = new Date().getDay();
+  // console.log(hr, min, sec, dt, mn, day);
+  // console.log();
+  let ampmt = 'AM';
+  if (hr > 12) {
+    hr = hr - 12;
+    ampmt = 'PM';
   }
-  if (sec < 10) {
-    sec = '0' + sec;
-  }
-  if (hour < 10) {
-    hour = '0' + hour;
-  }
-  if (min < 10) {
-    min = '0' + min;
-  }
-  let am = hour + ':' + min + ':' + sec + ' AM';
-  let pm = hour + ':' + min + ':' + sec + ' PM';
-  if (toggle) {
-    document.querySelector('.gettime').textContent = pm;
-    // document.getElementById("gettime").innerHTML = pm;
-    document.querySelector('.title').textContent = pm;
-  } else {
-    document.querySelector('.gettime').textContent = am;
-    // document.getElementById("gettime").innerHTML = am;
-    document.querySelector('.title').textContent = am;
-  }
-  document.querySelector('.date').textContent = date;
-}, 1000);
-
-const setBg = () => {
-  const randomColor1 = '#' + Math.floor(Math.random() * 16777215).toString(16);
-  const randomColor2 = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  hr = hr < 10 ? '0' + hr : hr;
+  min = min < 10 ? '0' + min : min;
+  sec = sec < 10 ? '0' + sec : sec;
+  ampm.innerText = ampmt;
+  hour.innerText = hr + ' :';
+  weekday.innerText = 'test';
+  minute.innerText = min + ' :';
+  second.innerText = sec + ' ';
+  date.innerText = dt + ' ' + monthNames[mn] + ',';
+  weekday.innerText = days[day];
+  setTimeout(() => {
+    updateDate();
+  }, 1000);
+}
+// setInterval(updateDate, 1000);
+updateDate();
+function setBg() {
+  const randomColor1 = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const randomColor2 = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   var r = document.querySelector(':root');
   // document.body.style.backgroundColor = '#' + randomColor1;
   console.log('clicked', randomColor1, randomColor2);
   r.style.setProperty('--primary-color', randomColor1);
   r.style.setProperty('--secondary-color', randomColor2);
-};
-const genNew = document.querySelector('.btn');
-genNew.addEventListener('click', e => {
-  console.log('u clicked me');
-});
-genNew.addEventListener('click', setBg);
+}
+document.querySelector('.btn').addEventListener('click', setBg);
